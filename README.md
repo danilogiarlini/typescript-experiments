e5b3bf4f4f2696d945ab1a8d68908b634768d1a6
danilogiarlini <117026136+danilogiarlini@users.noreply.github.com>
2023-05-12 13:49:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-experiments
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-hccv2z)
---
+++ index.ts
@@ -0,0 +1,10 @@
+class User {
+  id: number;
+  name: string;
+}
+
+const user: User = new User();
+
+user.id;
+
+console.log(user);
